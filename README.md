f5419c46e3e213f219821b6db370979e8d672f0c
John <mail@mrjohntan.com>
2022-10-08 10:14:26 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # api-sys
 API To Get Various System Info
+
+Steps to build Docker Image
+
+docker build -t fast123 .
+docker image tag fast123:latest mrjt/fast123:latest
+docker push mrjt/fast123:latest  
+
+docker run -d --name fast123 -p 80:80 fast123
---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import socket
+import random
+import platform
+
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+app = FastAPI()
+uname = platform.uname()
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/host")
+def read_host():
+    return {"Hostname": hostname, "IP": ip}
+
+
+@app.get("/random")
+def read_random():
+    return random.randrange(1, 1000000)
+
+@app.get("/system")
+def read_print():
+    return{"System": {uname.system}, "Node Name": {uname.node}, "Release": {uname.release}, "Version": {uname.version}, "Machine": {uname.machine}, "Processor": {uname.processor}}
+    
